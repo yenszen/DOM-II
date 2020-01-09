83a1e869bc44149b5dacc00eb8354057801970f1
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-01-09 22:55:06 +0800

    completed mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach(element => {
+    element.addEventListener("mouseover", (event) => {
+        event.target.style.color = "red";
+    });
+    element.addEventListener("mouseout", (event) => {
+        event.target.style.color = "black";
+    });
+});
+
