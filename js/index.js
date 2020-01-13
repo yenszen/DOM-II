e5974f9f1425f8de33b9219d63d88b52aaf08f01
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-01-13 22:41:54 +0800

    included event listeners such as dblclick, click, keydown and contextmenu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 // Your code goes here
+
+// VARIABLES
 const navLink = document.querySelectorAll(".nav-link");
+const header = document.querySelector(".main-navigation");
+const h2 = document.querySelector("h2");
+const p = document.querySelectorAll("p");
+const body = document.querySelector("body");
+const footer = document.querySelector("footer");
+
+// EVENTS
+// better clarity between menu options
 navLink.forEach(element => {
     element.addEventListener("mouseover", (event) => {
         event.target.style.color = "red";
@@ -8,4 +18,22 @@ navLink.forEach(element => {
         event.target.style.color = "black";
     });
 });
+
+// easier on the eyes
+body.addEventListener("dblclick", (event) => {
+    event.target.style.backgroundColor = "lightgrey";
+});
+body.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = ""; // reverts styling to previous state!
+})
+
+// dark mode
+body.addEventListener("keydown", (event) => {
+    event.target.style.color = "white";
+    event.target.style.backgroundColor = "black";
+})
+body.addEventListener("contextmenu", (event) => {
+    event.target.style.color = "";
+    event.target.style.backgroundColor = "";
+})
 
