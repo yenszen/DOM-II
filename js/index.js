adcac44632cc2faf84fbc9fde4d0aa524167c068
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-01-14 12:07:09 +0800

    added copy and dragend events, also included stopPropagation and preventDefault functionalities

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@
 
 // VARIABLES
 const navLink = document.querySelectorAll(".nav-link");
-const header = document.querySelector(".main-navigation");
 const h2 = document.querySelector("h2");
 const p = document.querySelectorAll("p");
 const body = document.querySelector("body");
-const footer = document.querySelector("footer");
+const img = document.querySelectorAll("img");
+const button = document.querySelectorAll(".btn");
+const header = document.querySelector(".main-navigation");
+const footer = document.querySelector(".footer");
 
 // EVENTS
 // better clarity between menu options
@@ -19,16 +21,16 @@ navLink.forEach(element => {
     });
 });
 
-// easier on the eyes
-body.addEventListener("dblclick", (event) => {
+// accessibility: easier on the eyes
+body.addEventListener("keydown", (event) => {
     event.target.style.backgroundColor = "lightgrey";
 });
-body.addEventListener("click", (event) => {
+body.addEventListener("keyup", (event) => {
     event.target.style.backgroundColor = ""; // reverts styling to previous state!
 })
 
 // dark mode
-body.addEventListener("keydown", (event) => {
+body.addEventListener("dblclick", (event) => {
     event.target.style.color = "white";
     event.target.style.backgroundColor = "black";
 })
@@ -37,3 +39,39 @@ body.addEventListener("contextmenu", (event) => {
     event.target.style.backgroundColor = "";
 })
 
+// illustrates which text is copied to clipboard
+p.forEach(element => {
+    element.addEventListener("copy", (event) => {
+        event.target.style.fontSize = "1.8rem";
+        event.target.style.color = "green";
+    });
+})
+
+img.forEach(element => {
+    element.addEventListener("dragend", (event) => {
+        event.target.style.visibility = "hidden";
+    })
+})
+
+// lack of signup page alert
+button.forEach(element => {
+    element.addEventListener("click", (event) => {
+        alert("Sign Up Page is under maintenance!");
+    })
+})
+
+// prevents nav items from refreshing the page
+navLink.forEach(element => {
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
+    })
+})
+
+// stop propagation
+header.addEventListener("dblclick", (event) => {
+    event.stopPropagation();
+})
+
+footer.addEventListener("dblclick", (event) => {
+    event.stopPropagation();
+})
